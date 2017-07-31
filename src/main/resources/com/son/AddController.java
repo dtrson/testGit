e52673e4e1fb413a140a6788dbf759366e03d1d8
Son Duong <Son Duong@192.168.0.52>
2017-07-31 22:54:12 +0200

    test commit

---
+++ src/main/resources/com/son/AddController.java
@@ -0,0 +1,13 @@
+package com.son;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AddController {
+
+	@RequestMapping("/add")
+	public void add() {
+		System.out.println("From the controller");
+	}
+}
